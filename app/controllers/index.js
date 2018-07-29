8c9701b627cc1a5adf0fc905299db32fb8320157
Robert Long <roblong@gmail.com>
2018-07-29 13:30:05 -0700

    init

---
+++ app/controllers/index.js
@@ -0,0 +1,26 @@
+import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
+  emailAddress: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+
+}); 
